92317f9c6d0c94fb3f458e922600fce5ba63048f
ShivamSh002 <shivamsharma98879@gmail.com>
2025-05-08 17:44:19 +0530

    api done - create task , get task , update task ,delete task

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
 const cors = require("cors");
 
 dotenv.config();
@@ -10,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/auth", authRoutes);
+app.use("/tasks", taskRoutes); 
 
 mongoose
   .connect(process.env.MONGO_URI)
